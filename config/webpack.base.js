26c5cdfd9a4d03e597aca19949a7679420b8367b
chengyaxiong <1281277038@qq.com>
2022-10-31 22:40:27 +0800

    feat: babel解析抽离成config文件

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -20,13 +20,7 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
+        use: ['babel-loader']
       },
       {
         test: /\.(css)$/,
